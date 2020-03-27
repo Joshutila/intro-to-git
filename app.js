3f5db5d4d622e835098666c9a67b8dae73611199
Joshua Mahon <thesoss52@gmail.com>
2020-03-27 17:28:36 +0000

    all files committed

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("This is my complex app");
+
+// git add adds a file to the git repository
+// git commit -m "Blah Blah Blah"
+// have to add the -m, its a message, a must have.
+// its a message to us to help us remember what we're saving.
+// have it in present tense.
+
+console.log("Another line!");
--- cats.js
+++ cats.js
@@ -1,2 +1,6 @@
 console.log("Meow");
 // just changed this file.
+// so we have to add it again.
+// then commit it again.
+
+console.log("Purrrr");
