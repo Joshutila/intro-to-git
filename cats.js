69309e42de58acc7bbced7e0f59fe014c3cbba5c
Joshua Mahon <thesoss52@gmail.com>
2020-03-30 15:55:10 +0000

    Committing new cats.js file

--- cats.js
+++ cats.js
@@ -1,6 +1,17 @@
-console.log("Meow");
-// just changed this file.
-// so we have to add it again.
-// then commit it again.
+console.log("This is my complex app");
 
-console.log("Purrrr");
+// git add adds a file to the git repository
+// git commit -m "Blah Blah Blah"
+// have to add the -m, its a message, a must have.
+// its a message to us to help us remember what we're saving.
+// have it in present tense.
+
+console.log("Another line!");
+
+// git status gives us what is going on at that time.
+
+// git init lets us start using git to being with.
+
+// git log lets us see everything that we have committed and when. when we press enter, we actually move down. we're in a different terminal now. to exit, press q.
+
+// git checkout. we copy the unique id that we see when we are in the git log, and we type git checkout, then that id.
